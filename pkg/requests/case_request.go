9a0392c8ca16c534d37d22df2d6e038819ea3d22
Ahingg <Xaviero.yamin28@gmail.com>
2025-07-15 23:18:41 +0700

    feat: changed create case to accept pdf files, changed the body to form-data

--- pkg/requests/case_request.go
+++ pkg/requests/case_request.go
@@ -3,7 +3,6 @@ package requests
 type CreateCaseRequest struct {
 	Name          string `json:"name" binding:"required"`
 	Description   string `json:"description"`
-	PDFFileUrl    string `json:"pdf_file_url"`
 	TimeLimitMs   int    `json:"time_limit_ms" binding:"required,min=1"`
 	MemoryLimitMb int    `json:"memory_limit_mb" binding:"required,min=1"`
 }
@@ -11,7 +10,6 @@ type CreateCaseRequest struct {
 type UpdateCaseRequest struct {
 	Name          string `json:"name" binding:"required"`
 	Description   string `json:"description"`
-	PDFFileUrl    string `json:"pdf_file_url"`
 	TimeLimitMs   int    `json:"time_limit_ms" binding:"required,min=1"`
 	MemoryLimitMb int    `json:"memory_limit_mb" binding:"required,min=1"`
 }
